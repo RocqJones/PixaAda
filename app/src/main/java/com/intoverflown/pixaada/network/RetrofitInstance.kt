51a6c2846cecf59dd4fe38eb715c8a7dbd18f15d
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-14 18:15:22 +0300

    network handler: retrofit Instance

--- app/src/main/java/com/intoverflown/pixaada/network/RetrofitInstance.kt
+++ app/src/main/java/com/intoverflown/pixaada/network/RetrofitInstance.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL + API_key)
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
