8e458092b23ad50927ab88953fd712bb246c6458
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-14 18:14:56 +0300

    modified constants

--- app/src/main/java/com/intoverflown/pixaada/network/ApiInterface.kt
+++ app/src/main/java/com/intoverflown/pixaada/network/ApiInterface.kt
@@ -1,11 +1,12 @@
 package com.intoverflown.pixaada.network
 
 import com.intoverflown.pixaada.data.DataImage
+import com.intoverflown.pixaada.utils.Constants
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiInterface {
     // parse api param query and create a data response setters and getters
-    @GET("&q=dogs")
+    @GET("?key=${Constants.API_key}&q=dogs")
     suspend fun fetchSearchData(): Response<DataImage>
 }
--- app/src/main/java/com/intoverflown/pixaada/utils/Constants.kt
+++ app/src/main/java/com/intoverflown/pixaada/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.intoverflown.pixaada.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "https://pixabay.com/api/?key="
         const val API_key = "24803204-6a5b3b36fbf3e86c5ef278665"
+        const val BASE_URL = "https://pixabay.com/api/"
     }
 }
