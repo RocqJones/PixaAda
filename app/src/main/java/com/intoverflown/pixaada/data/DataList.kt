b06542bf9d3cc91ac05b0d0c7919e4ca95a76009
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-15 12:47:47 +0300

    adapter datalist

---
+++ app/src/main/java/com/intoverflown/pixaada/data/DataList.kt
@@ -0,0 +1,32 @@
+package com.intoverflown.pixaada.data
+
+class DataList
+//    (
+//    collections: String,
+//    comment: String,
+//    id: String,
+//    largeImageURL: String,
+//    likes: String,
+//    previewHeight: String,
+//    previewURL: String,
+//    tags: String,
+//    user: String,
+//    userImageURL: String,
+//    user_id: String,
+//    views: String
+//)
+{
+    var collections: String? = null
+    var comments: String? = null
+    var id: String? = null
+    var largeImageURL: String? = null
+    var likes: String? = null
+    var previewHeight: String? = null
+    var previewURL: String? = null
+    var tags: String? = null
+    var user: String? = null
+    var userImageURL: String? = null
+    var user_id: String? = null
+    var views: String? = null
+}
+
