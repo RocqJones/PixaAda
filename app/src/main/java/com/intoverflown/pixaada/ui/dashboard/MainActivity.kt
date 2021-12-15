58902a54acdb65662cddcbe132bfdef3a01b1227
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-15 13:52:46 +0300

    dashboard

--- app/src/main/java/com/intoverflown/pixaada/ui/dashboard/MainActivity.kt
+++ app/src/main/java/com/intoverflown/pixaada/ui/dashboard/MainActivity.kt
@@ -1,27 +1,22 @@
 package com.intoverflown.pixaada.ui.dashboard
 
 import android.graphics.Paint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.intoverflown.pixaada.R
+import com.intoverflown.pixaada.data.DataList
+import com.intoverflown.pixaada.data.Hit
 import com.intoverflown.pixaada.databinding.ActivityMainBinding
 import com.intoverflown.pixaada.repository.Repository
+import com.intoverflown.pixaada.ui.adapter.AdapterMain
 import com.intoverflown.pixaada.ui.viewmodel.ViewModel
 import com.intoverflown.pixaada.ui.viewmodel.ViewModelFactory
-import AdapterMain
-import android.os.Build
-import android.view.View
-import android.widget.ImageView
-import androidx.annotation.RequiresApi
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
-import com.intoverflown.pixaada.data.Hit
-import org.json.JSONArray
-import org.json.JSONObject
-import java.util.ArrayList
-import kotlin.math.absoluteValue
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: ViewModel
 
     var adapterMain: AdapterMain? = null
-    private var dataHits: List<Hit>? = null
+    val list: ArrayList<DataList> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +34,16 @@ class MainActivity : AppCompatActivity() {
 
         binding!!.secTwoTxt.text = getString(R.string.popular_images) + " " + getString(R.string.popular_list)
 
-        // init recyclerView
-        dataHits = ArrayList<Hit>()
-        val linearLayoutManager = LinearLayoutManager(this)
-        binding!!.imgRecycler.layoutManager = linearLayoutManager
+        /**
+         * init recyclerView
+         */
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        binding!!.imgRecycler.layoutManager = gridLayoutManager
+        binding!!.imgRecycler.adapter = adapterMain
 
-        // initialize ViewModels
+        /**
+         * initialize ViewModels
+         */
         val repository = Repository()
         val viewModelFactory = ViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory)[ViewModel::class.java]
@@ -61,22 +60,52 @@ class MainActivity : AppCompatActivity() {
                 Log.d("responseTotalHits", response.body()?.totalHits.toString())
                 Log.d("responseHits", response.body()?.hits.toString())
 
-
-//                val passToAdapter: String = response.body()?.hits?.indexOf(Hit(collections = getString()).collections)
+//                Glide.with(binding!!.testGlide.context).load("https://cdn.pixabay.com/photo/2015/06/02/12/59/book-794978_150.jpg").into(binding!!.testGlide)
 
                 val passToAdapter: List<Hit>? = response.body()?.hits?.toMutableList()
                 Log.d("url", passToAdapter.toString())
+                /**
+                 * Iterate through Mutable JSONArray value of response [Hit] key
+                 */
                 for (i in passToAdapter!!.iterator()) {
                     val comment = i.comments
                     val previewURL = i.previewURL
+                    val collections = i.collections
+                    val id = i.id
+                    val largeImageURL = i.largeImageURL
+                    val likes = i.likes
+                    val previewHeight = i.previewHeight
+                    val tags = i.tags
+                    val user = i.user
+                    val userImageURL = i.userImageURL
+                    val user_id = i.user_id
+                    val views = i.views
                     Log.d("resM", "$comment : $previewURL")
-                    Log.d("resDataHits", dataHits.toString())
-//                    Glide.with(previewImg!!.context).load(previewURL).into(previewImg!!)
 
-//                    val dHits = Hit(
-//                        i.collections, i.comments, i.id, i.largeImageURL, i.likes, i.previewHeight,
-//                        i.previewURL, i.tags, i.user, i.userImageURL, i.user_id, i.views
-//                    )
+                    val dList : DataList = DataList()
+                    dList.collections = collections
+                    dList.comments = comment
+                    dList.id = id
+                    dList.largeImageURL = largeImageURL
+                    dList.likes = likes
+                    dList.previewHeight = previewHeight
+                    dList.previewURL = previewURL
+                    dList.tags = tags
+                    dList.user = user
+                    dList.userImageURL = userImageURL
+                    dList.user_id = user_id
+                    dList.views = views
+                    list.add(dList)
+                }
+
+                adapterMain = AdapterMain(list, this)
+                binding?.imgRecycler?.adapter = adapterMain
+
+                if (list.isNotEmpty()) {
+                    binding!!.loader.visibility = View.GONE
+                    binding!!.imgRecycler.visibility = View.VISIBLE
+                } else {
+                    binding!!.loader.visibility = View.VISIBLE
                 }
 
 //                dataHits = response.body()?.hits.toString())
@@ -100,13 +129,8 @@ class MainActivity : AppCompatActivity() {
 //                    )
 ////                    dataHits.add(dHits)
 //                }
-//                adapterMain = dataHits?.let { AdapterMain(it, this) }
+//                adapterMain = dataHits?.let { com.intoverflown.pixaada.ui.adapter.AdapterMain(it, this) }
 //                binding!!.imgRecycler.adapter = adapterMain
-
-//                val previewImg: ImageView = findViewById<View>(R.id.previewImage) as ImageView
-//                if (dataHits!!.isEmpty()) {
-//                    Glide.with(previewImg.context).load(R.drawable.ic_broken_image).into(previewImg)
-//                }
 
                 binding!!.totalSearchResults.text = "Found +" + response.body()?.total.toString() + " results"
                 binding!!.totalSearchResults.paintFlags = binding!!.totalSearchResults.paintFlags or Paint.UNDERLINE_TEXT_FLAG
