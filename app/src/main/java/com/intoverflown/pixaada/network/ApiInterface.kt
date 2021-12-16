cd3779ad15e31420540b8fa9f418b27e598ec4e6
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-16 20:24:24 +0300

    modified: changed fetch data param from @PATH to @Query

--- app/src/main/java/com/intoverflown/pixaada/network/ApiInterface.kt
+++ app/src/main/java/com/intoverflown/pixaada/network/ApiInterface.kt
@@ -1,23 +1,17 @@
 package com.intoverflown.pixaada.network
 
 import com.intoverflown.pixaada.data.DataImage
-import com.intoverflown.pixaada.ui.dashboard.MainActivity
 import com.intoverflown.pixaada.utils.Constants
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiInterface {
-    val searchString: String
-
     /**
      * parse api param query and call a data response model
      */
-
-    @GET("?key=${Constants.API_key}&q=dogs")
-//    Call<SearchData>
-    suspend fun fetchSearchData(): Response<DataImage>
-
-
-
+    @GET("?key=${Constants.API_key}&q=")
+    suspend fun fetchSearchData(@Query("searchStr") searchStr : String): Response<DataImage>
 }
