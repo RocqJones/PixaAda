5a08fb77a94b3cd5cfdc5da6602831827ff512cf
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-16 20:23:31 +0300

    modified: repository refactor

--- app/src/main/java/com/intoverflown/pixaada/repository/Repository.kt
+++ app/src/main/java/com/intoverflown/pixaada/repository/Repository.kt
@@ -1,14 +1,24 @@
 package com.intoverflown.pixaada.repository
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.intoverflown.pixaada.data.DataImage
+import com.intoverflown.pixaada.data.SearchData
+import com.intoverflown.pixaada.network.ApiInterface
 import com.intoverflown.pixaada.network.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
 
 class Repository {
-    // Fetch response from api
-    suspend fun fetchHitsData() : Response<DataImage> {
-        Log.d("resRepository", RetrofitInstance.api.fetchSearchData().toString())
-        return RetrofitInstance.api.fetchSearchData()
+
+    /**
+     * Fetch response from api
+     * Parse search string as param
+      */
+    suspend fun fetchHitsData(searchStr: String) : Response<DataImage> {
+        Log.d("resRepository", RetrofitInstance.api.fetchSearchData(searchStr).toString())
+        return RetrofitInstance.api.fetchSearchData(searchStr)
     }
 }
