60bf8e7146474af143199f78427737be4825ae3d
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-12-16 20:30:57 +0300

    import optimization

--- app/src/main/java/com/intoverflown/pixaada/repository/Repository.kt
+++ app/src/main/java/com/intoverflown/pixaada/repository/Repository.kt
@@ -1,14 +1,8 @@
 package com.intoverflown.pixaada.repository
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.intoverflown.pixaada.data.DataImage
-import com.intoverflown.pixaada.data.SearchData
-import com.intoverflown.pixaada.network.ApiInterface
 import com.intoverflown.pixaada.network.RetrofitInstance
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
 class Repository {
